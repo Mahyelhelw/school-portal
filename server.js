552709e1bf2c2684a2d5bfb34ebb284a9ae31fcd
Mahy <mahy.ibrahim02@eng-st.cu.edu.eg>
2026-04-26 01:54:01 +0300

    fix app not defined

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-// const express = require("express");
+ const express = require("express");
+ const app = express();
 // const mysql = require("mysql2");
 // const cors = require("cors");
 // require("dotenv").config();
